b7ab3752b8db6f27c414401460597f2153e5dc4d
QaziCoding <humd.qazi@city.ac.uk>
2024-10-26 23:04:10 +0100

    Add files via upload

---
+++ HW 4/task2.sql
@@ -0,0 +1,16 @@
+INSERT INTO petEvent (petname, eventdate, eventtype, remark)
+ VALUES ("Fluffy", "2020-10-15", "vet", "antibiotics");
+
+INSERT INTO petEvent (petname, eventdate, eventtype, remark)
+ VALUES ("Hammy", "2020-10-15", "vet", "antibiotics");
+
+UPDATE petEvent SET remark="5 kittens, 3 female, 2 male" WHERE petname="Fluffy";
+
+UPDATE petEvent SET petname="Claws" WHERE eventdate="1997-08-03";
+
+INSERT INTO petEvent (petname, eventdate, eventtype, remark)
+ VALUES ("Puffball", "2020-09-01", "death", NULL);
+
+DELETE FROM petPet WHERE petname="Buffy";
+
+DELETE FROM petEvent WHERE petname="Buffy";
