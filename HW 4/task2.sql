a91d2c6b164a3d0dfaf9d88749b48a6ef16ab208
QaziCoding <humd.qazi@city.ac.uk>
2024-10-27 13:34:18 +0000

    Update task2.sql

--- HW 4/task2.sql
+++ HW 4/task2.sql
@@ -4,7 +4,7 @@ INSERT INTO petEvent (petname, eventdate, eventtype, remark)
 INSERT INTO petEvent (petname, eventdate, eventtype, remark)
  VALUES ("Hammy", "2020-10-15", "vet", "antibiotics");
 
-UPDATE petEvent SET remark="5 kittens, 3 female, 2 male" WHERE petname="Fluffy";
+UPDATE petEvent SET remark="5 kittens, 3 female, 2 male" WHERE petname="Fluffy" AND eventtype= "litter";
 
 UPDATE petEvent SET petname="Claws" WHERE eventdate="1997-08-03";
 
@@ -14,3 +14,7 @@ INSERT INTO petEvent (petname, eventdate, eventtype, remark)
 DELETE FROM petPet WHERE petname="Buffy";
 
 DELETE FROM petEvent WHERE petname="Buffy";
+
+SELECT * FROM petPet;
+
+SELECT * FROM petEvent;
