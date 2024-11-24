92ca82cc9958218d85384e47f22626b007a1300f
QaziCoding <humd.qazi@city.ac.uk>
2024-11-24 18:37:08 +0000

    Add files via upload

---
+++ HW 5/task1.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS pet_database;
+CREATE DATABASE pet_database;
+USE pet_database;
+
+DROP TABLE IF EXISTS petPet, petEvent;
+
+-- Up to the next CUTOFF POINT should be in task1.sql
+
+CREATE TABLE petPet (
+  petname VARCHAR(20) NOT NULL,
+  owner VARCHAR(45),
+  species VARCHAR(45),
+  gender VARCHAR(1),
+  birth DATE,
+  death DATE,
+  PRIMARY KEY (petname,eventdate)
+);
+
+CREATE TABLE petEvent (
+  petname VARCHAR(20) NOT NULL,
+  eventdate DATE,
+  eventtype VARCHAR(20),
+  remark VARCHAR(255),
+  FOREIGN KEY petname REFERENCES petPet,
+  PRIMARY KEY ,
+);
+
