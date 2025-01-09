ffe533e45f86687e3cdd3c40247498a0f5dfa516
QaziCoding <humd.qazi@city.ac.uk>
2025-01-09 13:45:26 +0000

    Update task.sql
    wrong file uploaded

--- HW 7/task.sql
+++ HW 7/task.sql
@@ -1,69 +1,32 @@
-DROP DATABASE IF EXISTS pet_database;
-CREATE DATABASE pet_database;
-USE pet_database;
+-- 1
+CREATE VIEW samsBills AS
+(SELECT first_name, surname, bill_date, cust_name, bill_total
+FROM restStaff
+INNER JOIN restBill
+ON restStaff.staff_no = restBill.waiter_no
+WHERE staff_no = '004');
 
-DROP TABLE IF EXISTS petPet, petEvent;
+-- 2
+SELECT * FROM samsBills
+WHERE bill_total >= 400.00;
 
--- Up to the next CUTOFF POINT should be in task1.sql
+-- 3
+CREATE VIEW roomTotals (room_name, total_sum) AS 
+(SELECT room_name, sum(bill_total)
+FROM restRest_table R
+INNER JOIN restBill B
+ON  R.table_no = B.table_no
+GROUP BY room_name);
 
-CREATE TABLE petPet (
-  petname VARCHAR(20) NOT NULL,
-  owner VARCHAR(45),
-  species VARCHAR(45),
-  gender VARCHAR(1),
-  birth DATE,
-  death DATE,
-  PRIMARY KEY (petname)
-);
-
-CREATE TABLE petEvent (
-  petname VARCHAR(20) NOT NULL,
-  eventdate DATE NOT NULL,
-  eventtype VARCHAR(20),
-  remark VARCHAR(255),
-  FOREIGN KEY (petname) REFERENCES petPet(petname),
-  PRIMARY KEY (petname,eventdate)
-);
-
-INSERT INTO petPet VALUES
-    ("Fluffy", "Harold", "cat", "F", "1993-02-04", NULL),
-    ("Claws", "Gwen", "cat", "M", "1994-03-17", NULL),
-    ("Buffy", "Harold", "dog", "F", "1989-05-13", NULL),
-    ("Fang", "Benny", "dog", "M", "1990-08-27", NULL),
-    ("Bowser", "Diane", "dog", "M", "1979-08-31", "1995-07-29"),
-    ("Chirpy", "Gwen", "bird", "F", "1998-09-11", NULL),
-    ("Whistler", "Gwen", "bird", NULL, "1997-12-09", NULL),
-    ("Slim", "Benny", "snake", "M", "1996-04-29", NULL),
-    ('Puffball','Diane','hamster','F','1999-03-30',NULL),
-    ('Hammy','Diane','hamster','M','2010-10-30', NULL);
-
-INSERT INTO petEvent VALUES
-    ("Fluffy", "1995-05-15", "litter", "4 kittens, 3 female, 1 male"),
-    ("Buffy", "1993-06-23", "litter", "5 puppies, 2 female, 3 male"),
-    ("Buffy", "1994-06-19", "litter", "3 puppies, 3 female"),
-    ("Chirpy", "1999-03-21", "vet", "needed beak straightened"),
-    ("Slim", "1997-08-03", "vet", "broken rib"),
-    ("Slim", "1997-10-04", "vet", "broken tooth"),
-    ("Bowser", "1991-10-12", "kennel", NULL),
-    ("Fang", "1991-10-12", "kennel", NULL),
-    ("Fang", "1998-08-28", "birthday", "Gave him a new chew toy"),
-    ("Claws", "1998-03-17","birthday", "Gave him a new flea collar"),
-    ("Whistler", "1998-12-09", "birthday", "First birthday");
-
-INSERT INTO petEvent (petname, eventdate, eventtype, remark)
- VALUES ("Fluffy", "2020-10-15", "vet", "antibiotics");
-
-INSERT INTO petEvent (petname, eventdate, eventtype, remark)
- VALUES ("Hammy", "2020-10-15", "vet", "antibiotics");
-
-UPDATE petEvent SET remark="5 kittens, 3 female, 2 male" WHERE petname="Fluffy" AND eventtype="litter";
-
-UPDATE petEvent SET petname="Claws" WHERE eventdate="1997-08-03";
-
-INSERT INTO petEvent (petname, eventdate, eventtype, remark)
- VALUES ("Puffball", "2020-09-01", "death", NULL);
-
-DELETE FROM petEvent WHERE petname="Buffy";
+-- 4
+CREATE VIEW teamTotals (headwaiter_name, total_sum) AS
+(SELECT CONCAT(H.first_name, ' ', H.surname), sum(bill_total)
+FROM restStaff S
+INNER JOIN restStaff H
+ON H.staff_no = S.headwaiter
+INNER JOIN restBill B
+WHERE S.staff_no = B.waiter_no
+GROUP BY H.first_name, H.surname);
 
 DELETE FROM petPet WHERE petname="Buffy";
 
